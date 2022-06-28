abeda8ef44abccb4b3e51fcd2a088cdaf98100d3
Albin Forsberg <96afo02@gmail.com>
2022-06-28 13:54:20 +0200

    battle function implemented

--- app.js
+++ app.js
@@ -1,15 +1,49 @@
 let computerPlay = () => {
-    let hand;
-    switch(Math.floor(Math.random() * 3)) {
-        case 0:
-            hand = 'Rock';
-            break;
-        case 1:
-            hand = 'Paper';
-            break;
-        case 2:
-            hand = 'Scissor';
-            break;
+  let hand;
+  switch (Math.floor(Math.random() * 3)) {
+    case 0:
+      hand = "Rock";
+      break;
+    case 1:
+      hand = "Paper";
+      break;
+    case 2:
+      hand = "Scissor";
+      break;
+  }
+  return hand;
+};
+
+let battle = (playerSelection, computerSelection) => {
+  let result;
+  if (playerSelection === computerSelection) {
+    result = "draw";
+  } else if (playerSelection === "Rock") {
+    if (computerSelection === "Scissor") {
+      result = "win";
+    } else {
+      result = "lose";
     }
-    return hand;
-}
+  } else if (playerSelection === "Scissor") {
+    if (computerSelection === "Paper") {
+      result = "win";
+    } else {
+      result = "lose";
+    }
+  } else {
+    if (computerSelection === "Rock") {
+      result = "win";
+    } else {
+      result = "lose";
+    }
+  }
+
+  switch (result) {
+    case "win":
+      return "You win! " + playerSelection + " beats " + computerSelection;
+    case "lose":
+      return "You lose! " + computerSelection + " beats " + playerSelection;
+    default:
+      return "It's a draw.";
+  }
+};
